0ada866b9d4e7f4f930fa6233aa388588999980c
Steven Vachon <contact@svachon.com>
2026-08-16 17:54:22 -0400

    v3.0.1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import isRegExp from 'is-regexp';
 /**
  * Determine if a single match exists with an array of strings, numbers, and/or `RegExp`s.
  * @param {number | string} needle The value to find in `haystack`.
- * @param {(number | string | RegExp)[]} haystack The values to search.
+ * @param {ReadonlyArray<number | string | RegExp>} haystack The values to search.
  */
 export default (needle, haystack) =>
   haystack.some(strand =>
